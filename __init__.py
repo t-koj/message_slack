9c03f2aff94230a874fd61eed90c43ce5f511d9e
t-koj <t@kvi.jp>
2025-08-03 04:47:16 +0900

    update

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from src import post_message
--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import sys
 import os
+import asyncio
 from slack_sdk import WebClient
 from slack_sdk.web import SlackResponse
 from simple_parsing import parse_known_args, field
 from dataclasses import dataclass
 
-def send_message(message: str) -> SlackResponse:
+@dataclass
+class Arguments:
+    message: str = field(positional=True)
+
+async def post_message(message: str) -> SlackResponse:
     token = os.getenv("SLACKBOT_TOKEN")
     if not token:
         raise ValueError("Environment SLACKBOT_TOKEN is not set.")
@@ -13,20 +18,23 @@ def send_message(message: str) -> SlackResponse:
     if not channel:
         raise ValueError("Environment SLACKBOT_CHANNEL is not set.")
     client = WebClient(token=token)
+    client.async_mode = True
     return client.chat_postMessage(channel=channel, text=message)
 
 def _print_usage():
     print("Usage: python main.py <message>")
 
-@dataclass
-class Options:
-    message: str = field(positional=True)
+async def _main(args: Arguments):
+    message = args.message
+    if not message:
+        _print_usage()
+        sys.exit(1)
+    response = await post_message(message)
+    if response["ok"]:
+        print("Message sent successfully.")
+    else:
+        print(f"Failed to send message: {response['error']}")
 
 if __name__ == "__main__":
-    args = parse_known_args(Options)
-    if len(sys.argv) <= 1:
-        _print_usage()
-    else:
-        message = sys.argv[1]
-        send_message(message)
-    
+    args = parse_known_args(Arguments)
+    asyncio.run(_main(args))
